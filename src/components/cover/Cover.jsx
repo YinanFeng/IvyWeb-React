be2cf56dc41b624f34a35778aacc4f1aa3c58de5
Ivy Feng <ivy.feng@bcgdv.com>
2019-01-04 09:21:10 +1100

    fix frontPage

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LinkTags, {} from '../linkTags/LinkTags'
 
 class Cover extends React.Component{
     render(){
@@ -6,7 +7,12 @@ class Cover extends React.Component{
             <React.Fragment>
                 <h1 className='CoverPageTitle'>Ivy Yinan Feng</h1>
                 <p className='CoverPageTag'>Student - Developer - Avid Traveller</p>
-                <h3 onClick={() => this.props.onNavChoose('BIO')}>Explore More</h3>
+                <div className='exploreButtonDiv'>
+                    <h3 onClick={() => this.props.onNavChoose('BIO')} className='exploreButton'>Explore More</h3>
+                </div>
+                <div className='HomePageLinkTag'>
+                    <LinkTags />
+                </div>
             </React.Fragment>
         );
     };
--- src/components/linkTags/LinkTags.jsx
+++ src/components/linkTags/LinkTags.jsx
@@ -4,13 +4,12 @@ class LinkTags extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <hr/>
-                    <div>
+                    <div className='linkTag'>
                         <a href="https://www.facebook.com/yinan.feng.37"><img className="imageStyle" src={require('./FacebookLogo.png')} alt="image" /></a>
                         <a href="https://www.linkedin.com/in/yinan-feng/"><img className="imageStyle" src={require('./LinkedinLogo.png')} alt="image" /></a>
                         <a href="mailto:z5167277@ad.unsw.edu.au"><img className="imageStyle" src={require('./MailLogo.png')} alt="image" /></a>
                     </div>
-                <hr/>
+                {/* <hr/> */}
             </React.Fragment>
         )
     }
